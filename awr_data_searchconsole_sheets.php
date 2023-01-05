c0248d2e344404827b2523fc76751a25f69dd4e2
drili <DriliB@hotmail.com>
2023-01-05 11:33:35 +0100

    :hammer: Upaded clients array + script

--- awr_data_searchconsole_sheets.php
+++ awr_data_searchconsole_sheets.php
@@ -17,7 +17,10 @@
 
         // --- Array lists to run
         $array_projects = array(
-            array("www.fji.dk", "1-WhMxlGkAoOAQFytRokjyT7gKZGtTU9zHOZ981KCWYg", "Sheet1")
+            array("byravn.dk", "1sQeCk41INNKRnHrsfmSLbAnpS0ebXH7dM91rJ5TOsT0", "Sheet1"),
+            array("byravn.se", "1BS_2uOZKsupi1Ji2tVyPNFbCdiGBKENol83tIfikjdU", "Sheet1"),
+            array("byravn.no", "1QoJmFeJbk5hZW5ORryc0GEFU2yZeyr3H41OvdwDKj0Q", "Sheet1"),
+            array("vestermarkribe.dk", "1FQdmZAJFbb0T1hgKdJIgblag91y3BVJZYPg0Hrm3xZY", "Sheet1")
         );
 
         foreach ($array_projects as $key) {
@@ -28,100 +31,87 @@
             $project_sheet_name = $key[2];
 
             try {
-                function configureGoogleClient($project_sheet_url, $project_sheet_name) {
-                    // --- Configure the Google Client
-                    $client = new \Google_Client();
-                    $client->setApplicationName('Google Sheets API');
-                    $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
-                    $client->setAccessType('offline');
-                    $path = 'credentials.json';
-                    $client->setAuthConfig($path);
-    
-                    // - Init the Sheets Service
-                    $service = new \Google_Service_Sheets($client);
-                    
-                    // - Get the spreadsheet
-                    $spreadsheet_id = $project_sheet_url;
-                    $spreadsheet = $service->spreadsheets->get($spreadsheet_id);
-    
-                    // - Fetch all rows
-                    $range = $project_sheet_name;
-                    $response = $service->spreadsheets_values->get($spreadsheet_id, $range);
-                    $rows = $response->getValues();
-                    
-                    $headers = array_shift($rows);
-    
-                    // - Transform into assoc array
-                    $array = [];
-                    foreach ($rows as $row) {
-                        $array[] = array_combine($headers, $row);
-                    }
-    
-                    return $array;
+                // --- Configure the Google Client
+                $client = new \Google_Client();
+                $client->setApplicationName('Google Sheets API');
+                $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+                $client->setAccessType('offline');
+                $path = 'credentials.json';
+                $client->setAuthConfig($path);
+
+                // - Init the Sheets Service
+                $service = new \Google_Service_Sheets($client);
+                
+                // - Get the spreadsheet
+                $spreadsheet_id = $project_sheet_url;
+                $spreadsheet = $service->spreadsheets->get($spreadsheet_id);
+
+                // - Fetch all rows
+                $range = $project_sheet_name;
+                $response = $service->spreadsheets_values->get($spreadsheet_id, $range);
+                $rows = $response->getValues();
+                
+                $headers = array_shift($rows);
+
+                // - Transform into assoc array
+                $array = [];
+                foreach ($rows as $row) {
+                    $array[] = array_combine($headers, $row);
                 }
-                $array_ga_data = configureGoogleClient($project_sheet_url, $project_sheet_name);
+                $array_ga_data = $array;
     
-                function databaseTableHandler($project_name_sanitized, $con) {
-                    $table_suffix = $project_name_sanitized;
-                    if (mysqli_query($con, "DESCRIBE `awr_data_searchconsole_sheets_".$table_suffix."`")) {
-                        // - Table exists, empty table
-                        mysqli_query($con, "TRUNCATE TABLE `awr_data_searchconsole_sheets_".$table_suffix."`");
-                        echo "<script>console.log('SQL table already exists (awr_data_searchconsole_sheets_".$table_suffix."). - table emptied')</script>";
+                $table_suffix = $project_name_sanitized;
+                if (mysqli_query($con, "DESCRIBE `awr_data_searchconsole_sheets_".$table_suffix."`")) {
+                    // - Table exists, empty table
+                    mysqli_query($con, "TRUNCATE TABLE `awr_data_searchconsole_sheets_".$table_suffix."`");
+                    echo "<script>console.log('SQL table already exists (awr_data_searchconsole_sheets_".$table_suffix."). - table emptied')</script>";
+                } else {
+                    $sql_create_table = "CREATE TABLE `awr_data_searchconsole_sheets_".$table_suffix."` (
+                        unique_id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                        keyword VARCHAR(255) NOT NULL,
+                        clicks INT(11) NOT NULL,
+                        impressions INT(11) NOT NULL,
+                        ctr INT(11) NOT NULL,
+                        average_position INT(11) NOT NULL,
+                        project_client VARCHAR(255) NOT NULL
+                    )";
+
+                    if ($con->query($sql_create_table) === TRUE) {
+                        echo "<script>console.log('SQL table created successfully (awr_data_searchconsole_sheets_".$table_suffix.").')</script>";
                     } else {
-                        $sql_create_table = "CREATE TABLE `awr_data_searchconsole_sheets_".$table_suffix."` (
-                            unique_id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                            keyword VARCHAR(255) NOT NULL,
-                            clicks INT(11) NOT NULL,
-                            impressions INT(11) NOT NULL,
-                            ctr INT(11) NOT NULL,
-                            average_position INT(11) NOT NULL,
-                            project_client VARCHAR(255) NOT NULL
-                        )";
-    
-                        if ($con->query($sql_create_table) === TRUE) {
-                            echo "<script>console.log('SQL table created successfully (awr_data_searchconsole_sheets_".$table_suffix.").')</script>";
-                        } else {
-                            echo "ErrorResponse: Error creating table: " . $con->error;
-                        }
+                        echo "ErrorResponse: Error creating table: " . $con->error;
                     }
                 }
-                databaseTableHandler($project_name_sanitized, $con);
             } catch (\Throwable $th) {
                 throw $th;
             } finally {
-                echo "<pre>";
-                var_dump($array_ga_data);
-                echo "</pre>";
-                function handleGAData($array_ga_data, $project_name_sanitized, $con) {
-                    if (!empty($array_ga_data)) {
-                        $table = "awr_data_searchconsole_sheets_".$project_name_sanitized;
-                        
-                        foreach ($array_ga_data as $key) {
-                            $sql_insert = "INSERT INTO ".$table." (
-                                keyword, 
-                                clicks, 
-                                impressions,
-                                ctr,
-                                average_position,
-                                project_client) 
-                            VALUES (
-                                '".$key["Search query"]."', 
-                                '".$key["Clicks"]."',
-                                '".$key["Impressions"]."',
-                                '".$key["CTR"]."',
-                                '".$key["Average position"]."',
-                                '".$project_name_sanitized."'
-                            )";
+                if (!empty($array_ga_data)) {
+                    $table = "awr_data_searchconsole_sheets_".$project_name_sanitized;
+                    
+                    foreach ($array_ga_data as $key) {
+                        $sql_insert = "INSERT INTO ".$table." (
+                            keyword, 
+                            clicks, 
+                            impressions,
+                            ctr,
+                            average_position,
+                            project_client) 
+                        VALUES (
+                            '".mysqli_real_escape_string($con, $key["Search query"])."', 
+                            '".mysqli_real_escape_string($con, $key["Clicks"])."',
+                            '".mysqli_real_escape_string($con, $key["Impressions"])."',
+                            '".mysqli_real_escape_string($con, $key["CTR"])."',
+                            '".mysqli_real_escape_string($con, $key["Average position"])."',
+                            '".mysqli_real_escape_string($con, $project_name_sanitized)."'
+                        )";
 
-                            if ($con->query($sql_insert) === TRUE) {
-                                            
-                            } else {
-                                echo "Error: " . $sql_insert . "<br>" . $con->error;
-                            }
+                        if ($con->query($sql_insert) === TRUE) {
+                                        
+                        } else {
+                            echo "Error: " . $sql_insert . "<br>" . $con->error;
                         }
-                    }   
-                }
-                handleGAData($array_ga_data, $project_name_sanitized, $con);
+                    }
+                } 
             }
         }
     ?>
